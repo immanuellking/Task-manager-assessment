e09199cb6133f7f89b1905c4da085152da0b109f
immanuellking <emmaebubeegbosi@gmail.com>
2025-03-18 14:23:05 +0100

    feat: changes initial state structure so i cna add editing task state

--- src/store/tasksSlice.ts
+++ src/store/tasksSlice.ts
@@ -3,16 +3,23 @@ import { RootState } from "./store";
 export interface Task {
   id: string;
   title: string;
-  description: string;
-  status: "todo" | "in-progress" | "done";
+  description?: string;
+  status: "todo" | "in progress" | "done";
 }
-const initialState: Array<Task> = [];
+export interface TaskState {
+  tasks: Task[];
+  editingTask: Task | null;
+}
+const initialState: TaskState = {
+  tasks: [],
+  editingTask: null,
+};
 export const tasksSlice = createSlice({
-  name: "users",
+  name: "tasks",
   initialState,
   reducers: {
     addTask: (state, action: PayloadAction<Task>) => {
-      state.push(action.payload);
+      state.tasks.push(action.payload);
     },
   },
 });
