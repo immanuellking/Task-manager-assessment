946eef990babdb307aae631d19be2a3c8d37b245
immanuellking <emmaebubeegbosi@gmail.com>
2025-03-18 20:04:56 +0100

    feat: imppmeted functionality for filtering the tasks using the select drop down

--- src/components/tasks/TaskFilterBar.tsx
+++ src/components/tasks/TaskFilterBar.tsx
@@ -1,20 +1,42 @@
+import { useAppDispatch, useAppSelector } from "../../store/hook";
+import { Task, setFilterStatus, tasksSelector } from "../../store/tasksSlice";
+
 function TaskFilterBar() {
+  const dispatch = useAppDispatch();
+  const { filterStatus, tasks } = useAppSelector(tasksSelector);
+
+  const handleStatusChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    dispatch(
+      setFilterStatus(e.target.value as "to do" | "in progress" | "done")
+    );
+    // dispatch(filterTask(filter));
+  };
+
+  const completedTasks = tasks.filter(
+    (task: Task) => task.status === "done"
+  ).length;
+
   return (
     <div className="flex justify-between">
-        <div className="text-white">
-            1 of 3 Done
-        </div>
+      <div className="text-white">
+        {completedTasks} of {tasks.length} Done
+      </div>
 
-        <div>
-            <select name="filter" className="bg-[#4338ca] text-white text-center border border-slate-200 px-2 py-1 capitalize focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow">
-                <option value="">filter</option>
-                <option value="todo">to do</option>
-                <option value="inProgress">in progress</option>
-                <option value="done">done</option>
-            </select>
-        </div>
+      <div>
+        <select
+          name="filter"
+          className="bg-[#4338ca] text-white text-center border border-slate-200 px-2 py-1 capitalize focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+          value={filterStatus}
+          onChange={handleStatusChange}
+        >
+          <option value="all">ALL</option>
+          <option value="to do">to do</option>
+          <option value="in progress">in progress</option>
+          <option value="done">done</option>
+        </select>
+      </div>
     </div>
-  )
+  );
 }
 
-export default TaskFilterBar
+export default TaskFilterBar;
