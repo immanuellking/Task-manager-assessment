9eb411bf72e4caa1fa76477dbb0361fc9da91495
immanuellking <emmaebubeegbosi@gmail.com>
2025-03-18 08:18:58 +0100

    feat: added task lis component to dashboard

--- src/components/layout/Dashboard.tsx
+++ src/components/layout/Dashboard.tsx
@@ -1,9 +1,11 @@
+import TaskList from "../tasks/TaskList";
 import Header from "./Header";
 
 export default function Dashboard() {
   return (
     <div className="w-full min-h-screen bg-black">
         <Header />
+        <TaskList />
     </div>
   )
 }
