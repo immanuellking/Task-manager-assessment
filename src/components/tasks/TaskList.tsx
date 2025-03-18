057b207cc9e701b513e32b997d65478824e137fc
immanuellking <emmaebubeegbosi@gmail.com>
2025-03-18 08:55:02 +0100

    style: add responsive styles to task list

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -3,9 +3,9 @@ import TaskItem from "./TaskItem";
 
 export default function TaskList() {
   return (
-    <section className="px-4 lg:p-8">
+    <section className="px-4 py-8 lg:p-8 space-y-8">
         <TaskFilterBar />
-        <div className="flex gap-4 flex-wrap">
+        <div className="flex justify-center sm:gap-x-4 gap-y-6 flex-wrap">
             <TaskItem />
             <TaskItem />
             <TaskItem />
